3053dae689d24b40615c445f5e6c3f525d508a8e
chris.j.carter <chris.j.carter91@gmail.com>
2019-07-10 09:49:30 -0600

    Added missing Routes

--- data/router.js
+++ data/router.js
@@ -32,6 +32,36 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+
+  db.findPostComments(id)
+    .then(comments => {
+      if (comments && comments.length) {
+        res.status(200).json(comments);
+      } else {
+        res.status(404).json({
+          message: 'Cannot find comments for this post',
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const post = await db.insert(req.body);
+    console.log('post body: ', req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was an error adding the post',
+    });
+  }
+});
+
 router.post('/:id/comments', (req, res) => {
     if (!isValidComment(req.body)) {
       res.status(400).json({
